4ba5f33fc637de272c8f53472174366f57fe07e2
Jinhan_Bae <wlsgks333@naver.com>
2026-01-17 02:00:40 +0900

    style: CheckList 스타일 파일 추가

---
+++ src/components/checkList/styles.ts
@@ -0,0 +1,15 @@
+/**
+ * Variant별 컨테이너 스타일
+ */
+export const variantStyles = {
+  default: "px-4 py-3",
+  detail: "px-6 py-4 justify-center",
+};
+
+/**
+ * Variant별 텍스트 스타일
+ */
+export const textStyles = {
+  default: "text-base",
+  detail: "text-lg font-bold",
+};
