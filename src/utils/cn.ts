a18d08a306d3e528929a404899412144b19b7f9b
Jinhan_Bae <wlsgks333@naver.com>
2026-01-17 23:47:12 +0900

    chore: 주석 삭제

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -4,12 +4,7 @@ import { twMerge } from "tailwind-merge";
 /**
  * Tailwind CSS 클래스를 병합하는 유틸리티 함수
  *
- * clsx로 조건부 클래스를 결합하고, tailwind-merge로 충돌을 해결합니다.
- *
- * @example
- * cn("p-2", "p-4") // → "p-4" (충돌 해결)
- * cn("text-red-500", isActive && "text-blue-500") // 조건부 클래스
- * cn(baseStyles, className) // 외부 className으로 override 가능
+ * clsx로 조건부 클래스를 결합하고, tailwind-merge로 충돌을 해결
  */
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
